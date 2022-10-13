794637151000f6807777d6c7bf4b02a93ba98199
Emil Yangirov <emil@yangirov.ru>
2022-10-13 13:00:58 +0500

    feat: Add logger for http-requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Category } from './modules/category/category';
 import { HealthModule } from './modules/health/health.module';
 import { CategoryModule } from './modules/category/category.module';
+import { LoggerModule } from 'nestjs-pino';
+import { CategoryController } from './modules/category/category.controller';
 
 @Module({
   imports: [
@@ -22,6 +24,32 @@ import { CategoryModule } from './modules/category/category.module';
     }),
     CategoryModule,
     HealthModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        name: 'SOME BACKEND',
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        transport: {
+          targets: [
+            {
+              level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+              target: 'pino-loki',
+              options: {
+                batching: true,
+                interval: 5,
+                hostname: process.env.LOKI_URL,
+              },
+            },
+            {
+              target: 'pino-pretty',
+              options: {},
+              level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+            },
+          ],
+        },
+      },
+      forRoutes: [CategoryController],
+      exclude: [{ method: RequestMethod.ALL, path: 'health' }],
+    }),
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
@@ -17,10 +18,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document);
 
+  app.useLogger(app.get(Logger));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(PORT, () =>
+  await app.listen(PORT, async () =>
     console.log(`[Test API] started on port ${PORT}`),
   );
 }
